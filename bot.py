dd7c947e7e1cf37e10aa005561d150d2c6496f94
mbijanpour <mahanbijanpour@gmail.com>
2023-07-01 09:54:59 +0330

    completing docs for bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+"""
+    This Python code sets up a Telegram bot that can download videos from Reddit and send them to users.
+    email: mahanbjanpour@gmail.com
+    github:
+"""
+
+import telebot
+from redditDownloader import download
+import os
+
+
+# It initializes the Telegram bot with the provided token, which is a unique identifier for the bot.
+# This token is necessary for the bot to authenticate and interact with the Telegram API.
+bot = telebot.TeleBot(
+    token='6162340362:AAF49pkDUl7GN7SzvMN8rLbRSVam35bt0kw'
+)
+
+# this function handel the start command in the bot
+
+
+@bot.message_handler(commands=['start'])
+def send_start(message):
+    bot.send_message(
+        message.chat.id, 'Please send your link,and stand by.')
+
+# this function handel the given link and pass it to the redditDownloader.py to get the video
+
+
+@bot.message_handler(func=lambda message: True)
+def get_message(message):
+    chat_id = message.chat.id
+    text = message.text
+    video_path = download(text)
+    bot.send_video(chat_id=chat_id, video=open(video_path, 'rb'))
+    # for deleting the video after saving it for the user (optional)
+    os.remove(video_path)
+
+
+# starting the bot ...
+bot.infinity_polling()
--- redditDownloader.py
+++ redditDownloader.py
@@ -1,6 +1,6 @@
 import os
-from redvid import Downloader
 
+from redvid import Downloader
 
 
 def download(link: str) -> str:
@@ -8,14 +8,17 @@ def download(link: str) -> str:
     download the reddit video with the link passed to the argument and it will return the path of the
     downloaded video to bot.py
     """
+
     # Add the FFmpeg directory to the system's PATH environment variable
     ffmpeg_directory = "C:/ffmpeg-2023-06-26-git-285c7f6f6b-essentials_build/bin"
     os.environ['PATH'] += os.pathsep + ffmpeg_directory
-    reddit = Downloader(min_q=True)
+
+    # the process of getting the links downloading the video with redvid library
+    # link to the github repo: https://github.com/elmoiv/redvid
+    # the restrictions of the internet couldn't fit the max_q yet
+    reddit = Downloader(min_q=True) 
     reddit.url = link
-    reddit.path = 'D:/random py/reddit/vids'
-    print(os.path.basename(reddit.file_name))
+    reddit.path = 'D:/random py/reddit/vids' # path to the file for saving the videos
     video_path = reddit.download()
-    print(reddit.file_name)
-    # print(f"path:{video_path}")
-    return video_path
+    
+    return video_path # the video_path is the path of the downloaded file
